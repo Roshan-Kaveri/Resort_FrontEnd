39b342d835b61154f35772dba59d35fcb88338f3
Advaitaflek <58874950+Advaitaflek@users.noreply.github.com>
2024-04-03 23:59:12 +0530

    Add files via upload

---
+++ src/pages/Payment.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useLocation } from 'react-router-dom';
+import image from '../components/login_page/asssets/bg.png';
+function Payments() {
+    const location = useLocation();
+    const htmlContent = location.state?.htmlContent;
+    const encodeHTML = (html) => {
+        return html.replace(/[\u00A0-\u9999<>\&]/gim, function(i) {
+            return '' + i.charCodeAt(0) + ';';
+        });
+    };
+    return <div className="p_c">
+        <img src={image} alt="" />
+        <div className="p_rectangle1">
+            <h1 className="p_c1">
+                INVOICES
+            </h1>
+            <div className="p_textarea" style={{ paddingLeft: '1px' }}  dangerouslySetInnerHTML={{ __html: htmlContent }} /><div/>
+            <div className="p_rectangle"></div>
+            <div className='p_pass'>
+                <p><b>CARD NO:</b> <input type="password" id="p_password" name="password" required></input></p>
+            </div>
+            <div className='p_pass1'>
+        <b>CVV:</b> <input type="password" id="p_password1" name="password1" pattern="[0-9]{3}" title="Please enter a valid CVV (3 digits)"></input>
+                <b>EXPIRY:</b> <input type="text" name="text" id="p_text" required></input>
+            </div> <input className = "p_submit" type="submit" value="PAY NOW"></input></div>
+    </div>
+}
+export default Payments;
